a5e08391b715e007a48c757d52e0fba5c212bd67
neteoro <daviddoors@hotmail.com>
2024-01-26 14:12:38 -0300

    practica6 - Create AWS subnets

---
+++ practica_6/variables.tf
@@ -0,0 +1,19 @@
+variable "virginia_cidr" {
+  description = "CIDR Virginia"
+  type        = string
+  sensitive   = false
+
+
+}
+
+variable "public_subnet" {
+  description = "CIDR public subnet"
+  type        = string
+
+}
+
+variable "private_subnet" {
+  description = "CIDR private subnet"
+  type        = string
+
+}
