8775cc0bede6658998319f5cd23446250b177429
davidp <dpuertas@dcarsat.com.ar>
2024-03-16 09:04:02 -0300

    practica7 - Locals taging sufix S3

--- practica_7/locals.tf
+++ practica_7/locals.tf
@@ -1,3 +1,14 @@
 locals {
   sufix = "${var.tags.project}-${var.tags.env}-${var.tags.region}" #nomenclatura (recurso-cerberus-prod-region)
 }
+
+resource "random_string" "sufijo_s3" {
+  length  = 8
+  special = false
+  upper   = false
+
+}
+
+locals {
+  s3-sufix = "${var.tags.project}-${random_string.sufijo_s3.id}"
+}
